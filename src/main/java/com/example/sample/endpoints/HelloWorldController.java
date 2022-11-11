c66add0897c75d6a7d4f9a7b352d22f60a9402cb
Hima6 <himabindu.t6@gmail.com>
2022-11-11 14:49:24 +0530

    inital

---
+++ src/main/java/com/example/sample/endpoints/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.sample.endpoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/HelloWorld")
+public class HelloWorldController {
+    
+    @GetMapping
+    public String hello(){
+        return "success";
+    }
+}
